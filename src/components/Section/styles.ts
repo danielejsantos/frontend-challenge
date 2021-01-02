f54e8fcd61d6a14f238d698268dfa738076aa3af
danielejsantos <js_dani@hotmail.com>
2021-01-02 12:41:58 -0300

    Create section component

---
+++ src/components/Section/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0px 0px 8px 2px rgba(204,204,204,0.75);
+`;
