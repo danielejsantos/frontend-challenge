b238c7bb763826b1958b607893ff919a9be23326
danielejsantos <js_dani@hotmail.com>
2021-01-03 10:19:01 -0300

    Create Local section

---
+++ src/pages/Home/Local/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Section from "../../../components/Section";
+import SectionTitle from "../../../components/SectionTitle";
+
+const Local: React.FC = () => {
+  return (
+    <Section>
+      <SectionTitle title={"Local"} />
+    </Section>
+  );
+};
+
+export default Local;
