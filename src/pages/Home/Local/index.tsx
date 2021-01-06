a08df841c038470f12efc1338e268ac9e93049e8
danielejsantos <js_dani@hotmail.com>
2021-01-06 16:31:37 -0300

    Create Local section style and add map

--- src/pages/Home/Local/index.tsx
+++ src/pages/Home/Local/index.tsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
 
 import Section from "../../../components/Section";
 import SectionTitle from "../../../components/SectionTitle";
 
-import { Container } from "./styles";
+import { Container, AddressContainer, Text, MarkerIcon } from "./styles";
 
 const Local: React.FC = () => {
   return (
     <Container>
       <Section>
         <SectionTitle title="Local" />
+
+        <MapContainer
+          center={[-23.564769, -46.678009]}
+          zoom={15}
+          style={{ width: "100%", height: "80%" }}
+        >
+          <TileLayer
+            url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAP_TOKEN}`}
+          />
+        </MapContainer>
+        <AddressContainer>
+          <MarkerIcon />
+          <Text>Avenida Brasil, 4019 (Trabalho)</Text>
+        </AddressContainer>
       </Section>
     </Container>
   );
--- src/pages/Home/Local/styles.ts
+++ src/pages/Home/Local/styles.ts
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 export const Container = styled.div`
   grid-area: local;
+`;
+
+export const AddressContainer = styled.div`
+  display: flex;
+
+  @media (min-height: 680px) {
+    margin-top: 2px;
+  }
+`;
+
+export const Text = styled.p`
+  color: #5a5a5a;
+  font-size: 13px;
+`;
+
+export const MarkerIcon = styled(FaMapMarkerAlt)`
+  color: #5a5a5a;
+  width: 13px;
+  height: 13px;
+  margin-right: 5px;
 `;
