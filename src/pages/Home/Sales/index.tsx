7cc6151b4afc36f924af3b63d8bafb8b8122a14e
danielejsantos <js_dani@hotmail.com>
2021-01-06 16:37:20 -0300

    Add chart to Sales section

--- src/pages/Home/Sales/index.tsx
+++ src/pages/Home/Sales/index.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  XYPlot,
+  HorizontalGridLines,
+  VerticalGridLines,
+  XAxis,
+  YAxis,
+  LineSeries,
+} from "react-vis";
+
+import "react-vis/dist/style.css";
 
 import Section from "../../../components/Section";
 import SectionTitle from "../../../components/SectionTitle";
@@ -6,10 +16,24 @@ import SectionTitle from "../../../components/SectionTitle";
 import { Container } from "./styles";
 
 const Sales: React.FC = () => {
+  const data = [
+    { x: 0, y: 1 },
+    { x: 1, y: 3 },
+    { x: 2, y: 2 },
+    { x: 3, y: 4 },
+  ];
+
   return (
     <Container>
       <Section>
         <SectionTitle title="Vendas" />
+        <XYPlot height={200} width={250}>
+          <HorizontalGridLines />
+          <VerticalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineSeries data={data} />
+        </XYPlot>
       </Section>
     </Container>
   );
